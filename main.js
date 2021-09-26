84f1c373b0648ecca4b035e0788aa4635b0d2e35
bbggkkk <bbggkkk@naver.com>
2021-09-26 15:07:22 +0900

    콘솔 라인 삭제

--- main.js
+++ main.js
@@ -45,7 +45,6 @@
                 return new Function('return '+string.match(/\$\{(.*)\}/)[1])();
             }else{
                 return string.replace(/\$\{(.*)\}/g,(match,p1) => {
-                    console.log(p1);
                     return new Function('return '+p1)();
                 });
             }
